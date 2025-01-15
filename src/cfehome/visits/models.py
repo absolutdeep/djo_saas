9bf93692bd0e42719bebf7b213d11e7a437e4e62
deep <absolutdeep@gmail.com>
2025-01-15 00:06:11 -0500

    your_message

--- src/cfehome/visits/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Visits(models.Model):
-    path = models.TextField(null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
